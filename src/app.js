fe11b085fff6200811edaca2f769f9732ab5f2da
Ayushmaan <ayushmaankumar5@gmail.com>
2025-02-10 11:53:46 +0530

    trying to solve cros origin problem

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ const app = express()
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
+        origin: "http://localhost:5173",
+        credentials: true, // Allow cookies/auth headers if needed
+        methods: "GET,POST,PUT,DELETE,OPTIONS",
+        allowedHeaders: "Content-Type,Authorization",
     })
 )
 app.use(express.json({ limit: "16kb" }))
@@ -15,6 +17,21 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 app.use(morgan("dev"))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173")
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    )
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    res.header("Access-Control-Allow-Credentials", "true")
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204)
+    }
+
+    next()
+})
 
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
