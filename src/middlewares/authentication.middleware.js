4f6d0fcaa24a28c1dfe41e6144d55e5d816f63b5
Ayushmaan <ayushmaankumar5@gmail.com>
2025-05-10 02:26:08 +0530

    fix: add await to user retrieval in JWT verification middleware

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -13,7 +13,7 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
-        const user = await User.findById(decodedToken?._id).select(
+        const user = User.findById(decodedToken?._id).select(
             "-password -refreshToken"
         )
         if (!user) {
